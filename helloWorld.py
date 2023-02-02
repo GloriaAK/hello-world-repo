81f2e85e94dc5ec79dcac31a7007a276f457b198
Gloria Kolyabin <gloria.kluth@gmail.com>
2023-02-02 17:34:07 -0500

    Adding two numbers

--- helloWorld.py
+++ helloWorld.py
@@ -4,3 +4,10 @@ usrNm = input("What is your name? ")
 
 print("Hello " + usrNm)
 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+numSum = num1 + num2
+
+print("The sum of those two numbers is: " + str(numSum))
+
